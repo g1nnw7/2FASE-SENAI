b57f6c17ce658c3bdb0702efb298b91626117bf4
g1nnw7 <numseiquenomecoloca@gmail.com>
2025-04-09 21:44:02 -0300

    painel de oracao

---
+++ painel-de-oracoes/src/components/Oracao1.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+function Oracao1() {
+  return (
+    <div>
+      "Oh Grande Sintaxe,
+
+que tudo vê e tudo espera,
+
+ensinai-me a diferença entre `==` e `===`,
+
+entre `let`, `var`, e `const`,
+
+entre o que parece funcionar
+
+e o que realmente funciona.
+
+Dai-me humildade para perguntar,
+
+curiosidade para explorar,
+
+e persistência para continuar
+
+quando tudo der `undefined`.
+
+Pois estou apenas começando,
+
+mas já sou parte do repositório da evolução.
+
+**Commit inicial. Amém.**"
+    </div>
+  )
+}
+
+export default Oracao1
